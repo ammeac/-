3704d893745707edd5fe9183f925ef6db7a4fa04
Аммиак <prodiya@mail.ru>
2025-04-07 21:54:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,52 +1,116 @@
 ﻿using System;
 
-class Program
+namespace zadaniya
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Write("Введите первое число: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-
-
-        Console.Write("Введите операцию (+, -, *, /): ");
-        string operation = Console.ReadLine();
-
-
-        Console.Write("Введите второе число: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
-
-        double result = 0;
-
-        if (operation == "+")
+        static void Main(string[] args)
         {
-            result = num1 + num2;
-        }
-        else if (operation == "-")
-        {
-            result = num1 - num2;
-        }
-        else if (operation == "*")
-        {
-            result = num1 * num2;
-        }
-        else if (operation == "/")
-        {
-            if (num2 != 0)
+
+            // 1
+            static int Sum(List<int> num)
             {
-                result = num1 / num2;
+                int sum = 0;
+                foreach (int number in num)
+                {
+                    sum += number;
+                }
+                return sum;
             }
-            else
-            {
-                Console.WriteLine("Ошибка! Деление на ноль.");
-                return;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Неверная операция! Попробуйте снова.");
-            return;
-        }
 
-        Console.WriteLine($"Результат: {result}");
+            // 2
+            static bool palidram(string str)
+            {
+                str = str.ToLower();
+                for (int i = 0; i < str.Length / 2; i++)
+                {
+                    if (str[i] != str[str.Length - 1 - i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            // 3
+
+            static long factolial(int n)
+            {
+                long result = 1;
+                if (n == 0)
+                {
+                    return 1;
+                }
+                if (n < 0)
+                {
+                    Console.WriteLine("Неверно.");
+                }
+                int i = 1;
+                while (i <= n)
+                {
+                    result = result * i;
+                    i = i + 1;
+                }
+                return result;
+            }
+
+
+
+
+            // 4
+            static int Max(List<int> numbers)
+            {
+                if (numbers == null || numbers.Count == 0)
+                {
+                    Console.WriteLine("Списку нельзя быть пустым.");
+                    return 0;
+                }
+                int max = numbers[0];
+
+                int i = 1;
+
+                while (i < numbers.Count)
+                {
+
+                    if (numbers[i] > max)
+                    {
+
+                        max = numbers[i];
+                    }
+                    i = i + 1;
+                }
+                return max;
+            }
+
+
+
+            // 5
+            static List<int> deletedubl(List<int> numbers)
+            {
+                List<int> result = new List<int>();
+
+                foreach (int number in numbers)
+                {
+                    bool found = false;
+                    int i = 0;
+                    while (i < result.Count)
+                    {
+                        if (result[i] == number)
+                        {
+
+                            found = true;
+                            break;
+                        }
+                        i = i + 1;
+                    }
+
+                    if (!found)
+                    {
+                        result.Add(number);
+                    }
+                }
+                return result;
+            }
+        }
     }
 }
+
